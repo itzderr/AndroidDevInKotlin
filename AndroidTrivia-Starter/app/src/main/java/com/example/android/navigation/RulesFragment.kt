937526775121a5c2241c7eb06af7fb0b6a524267
Derrick Park <derrick8689@gmail.com>
2019-12-13 16:01:18 -0800

    Add an options menu 3.2

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/RulesFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/RulesFragment.kt
@@ -17,10 +17,10 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 
 class RulesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
