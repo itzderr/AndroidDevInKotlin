7c7d6becdd76f3d7601e3377417f44f77ca141a8
Derrick Park <derrick8689@gmail.com>
2019-12-16 16:16:48 -0800

    Add drawer and share implicit intent

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -62,7 +62,8 @@ class GameFragment : Fragment() {
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<String>
     private var questionIndex = 0
-    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+//    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+    private val numQuestions = 1
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -101,12 +102,12 @@ class GameFragment : Fragment() {
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
                         view.findNavController()
-                                .navigate(R.id.action_gameFragment_to_gameWonFragment)
+                                .navigate(GameFragmentDirections.actionGameFragmentToGameWonFragment(numQuestions, questionIndex))
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
                     view.findNavController()
-                            .navigate(R.id.action_gameFragment_to_gameOverFragment2)
+                            .navigate(GameFragmentDirections.actionGameFragmentToGameOverFragment2())
                 }
             }
         }
--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,10 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -33,8 +33,47 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener {
-            it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            it.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "${args.numCorrect} / ${args.numQuestions}", Toast.LENGTH_SHORT).show()
+        // add an options menu
+        setHasOptionsMenu(true)
+
         return binding.root
     }
+
+    private fun getShareIntent(): Intent {
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        // implicit intent
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain") // MIME TYPE
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "${args.numCorrect}/${args.numQuestions}")
+        return shareIntent
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.winner_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.share -> startActivity(getShareIntent())
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
